c4368a4091d5e4e823ba838ba2fdac397195f92c
Victor Kauan <victorkauan.dev@gmail.com>
2023-02-06 23:20:53 -0300

    feat: redirect with useNavigate hook

--- src/pages/ContactDetails.tsx
+++ src/pages/ContactDetails.tsx
@@ -1,11 +1,18 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export function ContactDetails() {
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  function handleClick() {
+    console.log('Message sent successfully!');
+    return navigate('/');
+  }
 
   return (
     <div>
       <h1>Showing more information for contact: {id}</h1>
+      <button onClick={handleClick}>Send message</button>
     </div>
   );
 }
